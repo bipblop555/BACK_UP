0512fcbd3598994221378eec6c9149cbf43c796e
Rayane <101555619+Hikyy@users.noreply.github.com>
2023-04-25 11:40:23 +0200

    Update dump

--- dump.sql
+++ dump.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.19  Distrib 10.3.38-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: user_linux
+-- Host: localhost    Database: APEX
 -- ------------------------------------------------------
 -- Server version	10.3.38-MariaDB-0ubuntu0.20.04.1
 
